88142c391d940a02ba4a3dfcc1ad31568220cd64
Bezawada Uditha <140940352+BezawadaUditha@users.noreply.github.com>
2025-05-23 14:30:25 +0530

    Update list-users.sh

--- list-users.sh
+++ list-users.sh
@@ -12,14 +12,13 @@ REPO_OWNER=$1
 REPO_NAME=$2
 
 # Function to make a GET request to the GitHub API
-#function github_api_get {
- #   local endpoint="$1"
-  #  local url="${API_URL}/${endpoint}"
+function github_api_get {
+    local endpoint="$1"
+    local url="${API_URL}/${endpoint}"
 
-   # # Send a GET request to the GitHub API with authentication
-    #curl -s -u "${USERNAME}:${TOKEN}" "$url"
-#}
-
+    # Send a GET request to the GitHub API with authentication
+    curl -s -u "${USERNAME}:${TOKEN}" "$url"
+}
 # Function to list users with read access to the repository
 function list_users_with_read_access {
     local endpoint="repos/${REPO_OWNER}/${REPO_NAME}/collaborators"
@@ -35,18 +34,18 @@ function list_users_with_read_access {
         echo "$collaborators"
     fi
 }
-function helper {
-    expected_cmd_args=2
+#function helper {
+ #   expected_cmd_args=2
 
-    if [ $# -ne $expected_cmd_args ]; then
-        echo "Please execute the script with required command-line arguments"
-        echo "Usage: ./script.sh <arg1> <arg2>"
-        return 1
-    fi
+  #  if [ $# -ne $expected_cmd_args ]; then
+   #     echo "Please execute the script with required command-line arguments"
+    #    echo "Usage: ./script.sh <arg1> <arg2>"
+     #   return 1
+    #fi
 
-    echo "Function executed with correct number of arguments."
+    #echo "Function executed with correct number of arguments."
     # Additional logic can go here
-}
+#}
 
 # Main script
 
